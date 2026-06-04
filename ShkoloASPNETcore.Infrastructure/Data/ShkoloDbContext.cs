55edef6ef892188884d5949431d645d16a25dd11
toman <toma.neshkov.highschool@buditel.bg>
2026-06-04 22:07:15 +0300

    Add Db Context

---
+++ ShkoloASPNETcore.Infrastructure/Data/ShkoloDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShkoloASPNETcore.Infrastructure.Models;
+using ShkoloASPNETcore.Infrastructure.Models.ShkoloClone.Infrastructure.Data.Models;
+
+namespace ShkoloASPNETcore.Infrastructure.Data
+{
+    public class ShkoloDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ShkoloDbContext(DbContextOptions<ShkoloDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; } = null!;
+        public DbSet<Teacher> Teachers { get; set; } = null!;
+        public DbSet<Subject> Subjects { get; set; } = null!;
+        public DbSet<Grade> Grades { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
