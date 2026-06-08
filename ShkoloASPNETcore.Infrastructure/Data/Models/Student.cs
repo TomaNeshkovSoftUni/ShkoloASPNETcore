1dad08ec98d39a9cd7a3e2614358229f3c276a84
toman <toma.neshkov.highschool@buditel.bg>
2026-06-08 22:59:40 +0300

    fix formatting and syntax

--- ShkoloASPNETcore.Infrastructure/Data/Models/Student.cs
+++ ShkoloASPNETcore.Infrastructure/Data/Models/Student.cs
@@ -7,29 +7,29 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ShkoloASPNETcore.Infrastructure.Data.Models
 {
-        public class Student
-        {
-            [Key]
-            public int Id { get; set; }
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
 
-            [Required]
-            [MaxLength(50)]
-            public string FirstName { get; set; } = null!;
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; } = null!;
 
-            [Required]
-            [MaxLength(50)]
-            public string LastName { get; set; } = null!;
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; } = null!;
 
-            [Required]
-            public string ApplicationUserId { get; set; } = null!;
+        [Required]
+        public string ApplicationUserId { get; set; } = null!;
 
-            [ForeignKey(nameof(ApplicationUserId))]
-            public ApplicationUser ApplicationUser { get; set; } = null!;
+        [ForeignKey(nameof(ApplicationUserId))]
+        public ApplicationUser ApplicationUser { get; set; } = null!;
 
-            [Required]
-            [MaxLength(20)]
-            public string EnrollmentNumber { get; set; } = null!;
+        [Required]
+        [MaxLength(20)]
+        public string EnrollmentNumber { get; set; } = null!;
 
-            public ICollection<Grade> Grades { get; set; } = new List<Grade>();
-        }
+        public ICollection<Grade> Grades { get; set; } = new List<Grade>();
+    }
 }
