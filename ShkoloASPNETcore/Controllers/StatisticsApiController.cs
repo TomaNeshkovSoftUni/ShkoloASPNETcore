11c34607f381f66bac0946809a7bb6370b85a3b9
toman <toma.neshkov.highschool@buditel.bg>
2026-06-09 22:02:37 +0300

    Add first API + AJAX request + teacher auth

---
+++ ShkoloASPNETcore/Controllers/StatisticsApiController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShkoloASPNETcore.Infrastructure.Data;
+
+namespace ShkoloASPNETcore.Web.Controllers
+{
+    [ApiController]
+    [Route("api/statistics")]
+    public class StatisticsApiController : ControllerBase
+    {
+        private readonly ShkoloDbContext _context;
+
+        public StatisticsApiController(ShkoloDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCounts()
+        {
+            var totalStudents = await _context.Students.CountAsync();
+            var totalTeachers = await _context.Teachers.CountAsync();
+
+            return Ok(new { students = totalStudents, teachers = totalTeachers });
+        }
+    }
+}
--- ShkoloASPNETcore/Controllers/TeacherController.cs
+++ ShkoloASPNETcore/Controllers/TeacherController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using ShkoloASPNETcore.Infrastructure.Data.Models;
 
 namespace ShkoloASPNETcore.Web.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class TeacherController : Controller
     {
         private readonly ShkoloDbContext _context;
