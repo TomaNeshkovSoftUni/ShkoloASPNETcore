69a487d8ad6ff8ad441779e04cbc56e095f9c811
toman <toma.neshkov.highschool@buditel.bg>
2026-06-10 21:46:18 +0300

    make dumb scaffolding mistakes

--- ShkoloASPNETcore.Infrastructure/Data/Models/ApplicationUser.cs
+++ ShkoloASPNETcore.Infrastructure/Data/Models/ApplicationUser.cs
@@ -7,16 +7,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ShkoloASPNETcore.Infrastructure.Data.Models
 {
-
-    [Table("Users")] // tell ef to map this model to Users table (not AspNetUsers)
+    [Table("Users")]
     public class ApplicationUser : IdentityUser
     {
         [Required]
         [MaxLength(50)]
-        public string FirstName { get; set; } = null!;
+        public string FirstName { get; set; } = string.Empty;
 
         [Required]
         [MaxLength(50)]
-        public string LastName { get; set; } = null!;
+        public string LastName { get; set; } = string.Empty;
     }
 }
