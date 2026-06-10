e5634b9eb4dbd9c403261a034414b37c9c3ddbca
toman <toma.neshkov.highschool@buditel.bg>
2026-06-10 19:04:05 +0300

    remake remark controller and implement custom global application error handling + controller

---
+++ ShkoloASPNETcore/Controllers/ErrorsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShkoloASPNETcore.Web.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        public IActionResult Error404()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+
+        public IActionResult Error401()
+        {
+            Response.StatusCode = 401;
+            return View();
+        }
+
+        public IActionResult Error400()
+        {
+            Response.StatusCode = 400;
+            return View();
+        }
+
+        public IActionResult Error500()
+        {
+            Response.StatusCode = 500;
+            return View();
+        }
+    }
+}
